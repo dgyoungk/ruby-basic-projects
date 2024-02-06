4924c76f171d273c113036e77d96cdd40ed7442f
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-02-06 14:00:00 -0800

    chore(CaesarCipher): implement class for rspec
    Class CaesarCipher was added to check method functionality.
    The ciphering method was also refactored to read easier
    and for modularity.

--- caesar-cipher/c_cipher.rb
+++ caesar-cipher/c_cipher.rb
@@ -1,36 +1,34 @@
-# Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string
+# caesar-cipher/c_cipher.rb
 
+# Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string
 # Take a string, split it up to ind. char, modify it to a number to perform arithmetics
 # take the shift key value, and add it to the char, convert back to char, join it back together and return it
-
 # gotchas: a-z looping (solve with conditionals [value checking])
 
-def cipherize(string, shift)
-  # get ind. chars, convert them into ASCII values and shift them
-  modified_nums = string.split('').map do |ch|
-    num = ch.ord
-    # testing for non-alpha ASCII values
-    if num < 'A'.ord
-      num
-    # testing for uppercase letters
-    elsif num.between?('A'.ord, 'Z'.ord)
-      if num + shift > 'Z'.ord
-        num = num + (shift - 26)
-      else
-        num += shift
-      end
-    # testing for lowercase letters
-    elsif num.between?('a'.ord, 'z'.ord)
-      if num + shift > 'z'.ord
-        num = num + (shift - 26)
-      else
-        num += shift
-      end
-    end
+class CaesarCipher
+
+  def convert_to_nums(string)
+    return string.split('').map { |ch| ch = ch.ord }
   end
 
-  modified_str = modified_nums.map { |num| num.chr }.join('')
-end
+  def convert_to_code(nums)
+    return nums.map { |num| num.chr }.join('')
+  end
 
-# method testing
-p cipherize(%(Testing with spaces and punctuation!), 10)
+  def cipherize(string, shift = 5)
+    # get ind. chars, convert them into ASCII values and shift them
+    modified_nums = convert_to_nums(string)
+    modified_nums.map! do |num|
+      unless num < 'A'.ord
+        if num.between?('A'.ord, 'Z'.ord)
+          num = num + shift > 'Z'.ord ? num + (shift - 26) : num + shift
+        elsif num.between?('a'.ord, 'z'.ord)
+          num = num + shift > 'z'.ord ? num + (shift - 26) : num + shift
+        end
+      else
+        num
+      end
+    end
+    convert_to_code(modified_nums)
+  end
+end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -5,12 +5,6 @@ require './caesar-cipher/c_cipher'
 describe CaesarCipher do
   subject { described_class.new }
 
-  describe '#cipherize' do
-    it "outputs the ciphered text" do
-      expect(subject.cipherize("Testing", 10)).to eql("Docdsxq")
-    end
-  end
-
   context 'when converting' do
     context 'from letters to numers' do
       it 'should have no alphabet characters' do
@@ -20,6 +14,24 @@ describe CaesarCipher do
     context 'from numbers to letters' do
       it 'should not contain any numbers' do
         expect(subject.convert_to_code(subject.convert_to_nums("Testing"))).not_to match(/\d/)
+      end
+    end
+  end
+
+  describe '#cipherize' do
+    context 'when given a shift value' do
+      it "ciphers the text according to the shift value" do
+        expect(subject.cipherize("Testing", 10)).to eql("Docdsxq")
+      end
+    end
+    context 'when not given a shit value' do
+      it 'ciphers the text based on a shift value of 5' do
+        expect(subject.cipherize("Testing")).to eql('Yjxynsl')
+      end
+    end
+    context 'when the string has whitespace and punctuation' do
+      it 'ciphers only the letters' do
+        expect(subject.cipherize("Testing with spaces!!!", 10)).to eql("Docdsxq gsdr czkmoc!!!")
       end
     end
   end
